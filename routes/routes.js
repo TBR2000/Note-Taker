1c24f4b213e32ef667ac87cd5cf8b708e9437805
Tim <SESA493999@se.com>
2021-05-17 18:36:42 +1000

    bug fixes

--- routes/routes.js
+++ routes/routes.js
@@ -8,11 +8,11 @@ const uniqid = require('uniqid');
 module.exports = (app) => {
   // GET handling
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
   app.get('/api/notes', (req, res) => {
-    let data = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     res.json(data);
   });
 
@@ -20,9 +20,9 @@ module.exports = (app) => {
   app.post("/api/notes", function(req, res) {
     let newNote = req.body;
     newNote.id = uniqid
-    let data = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     notes.push(newNote);
-    fs.writeFileSync('../db/db.json', JSON.stringify(data));
+    fs.writeFileSync('./db/db.json', JSON.stringify(data));
     res.JSON(data);
     
   });
@@ -30,15 +30,15 @@ module.exports = (app) => {
   // DELETE handling
   app.delete("/api/notes/:id", (req, res) => {
     let noteId = req.params.id.toString();
-    let data = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     const updateData = data.filter( note => note.id.toString()!== noteId);
-    fs.writeFileSync('../db/db.json', JSON.stringify(updateData));
+    fs.writeFileSync('./db/db.json', JSON.stringify(updateData));
     res.json(updateData);
   });
 
  // If no matching route is found default to home
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
 
